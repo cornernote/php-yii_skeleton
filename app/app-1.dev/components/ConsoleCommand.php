0893cc56156d7d6851fcc7cffb9785d57348143c
Brett O'Donnell <cornernote@gmail.com>
2013-07-09 19:23:21 +0930

    update console command

--- app/app-1.dev/components/ConsoleCommand.php
+++ app/app-1.dev/components/ConsoleCommand.php
@@ -11,6 +11,11 @@ class ConsoleCommand extends CConsoleCommand
     private static $_instances = array();
 
     /**
+     * @var
+     */
+    private $timer;
+
+    /**
      * @static
      * @param string $class
      * @return ConsoleCommand
@@ -28,8 +33,24 @@ class ConsoleCommand extends CConsoleCommand
      */
     public function init()
     {
+        $this->timer = microtime(true);
         PageTrail::model()->findCurrent();
         parent::init();
+    }
+
+
+    /**
+     * @param $i
+     * @param $count
+     * @return string
+     */
+    protected function runStats($i, $count)
+    {
+        $stats = '';
+        $stats .= '[mem=' . number_format(memory_get_peak_usage() / 1024 / 1024, 1) . '|' . number_format(memory_get_usage() / 1024 / 1024, 1) . ']';
+        $stats .= '[time=' . number_format((microtime(true) - $this->timer), 1) . ']';
+        $stats .= '[done=' . floor($i / $count * 100) . '%|' . $i . '/' . $count . ']';
+        return $stats . ' ';
     }
 
 }
