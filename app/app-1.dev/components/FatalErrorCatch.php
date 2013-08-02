194393ab6d2a6dfb9f847527766980acc4803657
Brett O'Donnell <cornernote@gmail.com>
2013-08-03 00:16:41 +0930

    added new theme

--- app/app-1.dev/components/FatalErrorCatch.php
+++ app/app-1.dev/components/FatalErrorCatch.php
@@ -16,35 +16,39 @@
  */
 class FatalErrorCatch extends CApplicationComponent
 {
-	/**
-	 * Yii-action for error displaying.
-	 * Better to use handlers from Yii because self-written handlers can have errors too :)
-	 * @var mixed
-	 */
-	public $errorAction = null;
-	/**
-	 * Errors types that we want to catch
-	 * @var array
-	 */
-	public $errorTypes = array(E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR, E_COMPILE_WARNING);
 
-	public function init()
-	{
-		register_shutdown_function(array($this, 'shutdownHandler'));
-		return parent::init();
-	}
+    /**
+     * Yii-action for error displaying.
+     * Better to use handlers from Yii because self-written handlers can have errors too :)
+     * @var mixed
+     */
+    public $errorAction = null;
 
-	/**
-	 * Error handler
-	 */
-	public function shutdownHandler()
-	{
-		$e = error_get_last();
-		if ($e !== null && in_array($e['type'], $this->errorTypes))
-		{
-			$msg = 'Fatal error: ' . $e['message'];
-			Yii::app()->errorHandler->errorAction = $this->errorAction;
-			Yii::app()->handleError($e['type'], $msg, $e['file'], $e['line']);
-		}
-	}
+    /**
+     * Errors types that we want to catch
+     * @var array
+     */
+    public $errorTypes = array(E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR, E_COMPILE_WARNING);
+
+    /**
+     * @return mixed
+     */
+    public function init()
+    {
+        register_shutdown_function(array($this, 'shutdownHandler'));
+        return parent::init();
+    }
+
+    /**
+     * Error handler
+     */
+    public function shutdownHandler()
+    {
+        $e = error_get_last();
+        if ($e !== null && in_array($e['type'], $this->errorTypes)) {
+            $msg = 'Fatal error: ' . $e['message'];
+            Yii::app()->errorHandler->errorAction = $this->errorAction;
+            Yii::app()->handleError($e['type'], $msg, $e['file'], $e['line']);
+        }
+    }
 }
