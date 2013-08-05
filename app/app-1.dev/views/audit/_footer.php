195c2ed335954355fd94b76d1acf8a6cf5a3b275
Brett O'Donnell <cornernote@gmail.com>
2013-08-05 14:13:32 +0930

    improve audit footer output

--- app/app-1.dev/views/audit/_footer.php
+++ app/app-1.dev/views/audit/_footer.php
@@ -2,12 +2,11 @@
 $audit = Audit::model()->findCurrent();
 if (!$audit) return;
 
-echo '<div' . (YII_DEBUG ? '' : ' style="color: #fff;"') . '>';
-echo '<!-- pt start -->pt-' . $audit->id . '<!-- pt end -->'; // html comment is used for extracting audit_id
-echo ' | ';
-echo Setting::item('app_version');
+$color = isset($color) ? $color : (YII_DEBUG ? 'inherit' : 'transparent');
+echo '<span style="color: ' . $color . ';">';
+echo '<!-- audit start -->audit-' . $audit->id . '<!-- audit end -->'; // html comment is used for extracting audit_id
 echo ' | ';
 echo number_format(microtime(true) - $audit->start_time, 2) . 'sec';
 echo ' | ';
 echo round(memory_get_peak_usage() / 1024 / 1024, 2) . 'mb';
-echo '</div>';
+echo '</span>';
