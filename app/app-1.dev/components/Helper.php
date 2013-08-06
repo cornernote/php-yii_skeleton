18f35dead49cec5d6cabfc61c732abdd71bbd641
Brett O'Donnell <cornernote@gmail.com>
2013-08-06 19:36:51 +0930

    added zip to helper

--- app/app-1.dev/components/Helper.php
+++ app/app-1.dev/components/Helper.php
@@ -74,4 +74,43 @@ class Helper
         return (Yii::app()->getDb()->createCommand("SHOW TABLES LIKE '" . $table . "'")->queryScalar() == $table);
     }
 
+    /**
+     * @param $source
+     * @param $destination
+     * @return bool
+     */
+    public static function zip($source, $destination)
+    {
+        if (!extension_loaded('zip') || !file_exists($source)) {
+            return false;
+        }
+
+        $zip = new ZipArchive();
+        if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+            return false;
+        }
+
+        $source = str_replace('\\', '/', realpath($source));
+
+        if (is_dir($source) === true) {
+            $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+            foreach ($files as $file) {
+                $file = str_replace('\\', '/', realpath($file));
+
+                if (is_dir($file) === true) {
+                    $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+                }
+                else if (is_file($file) === true) {
+                    $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+                }
+            }
+        }
+        else if (is_file($source) === true) {
+            $zip->addFromString(basename($source), file_get_contents($source));
+        }
+
+        return $zip->close();
+    }
+
 }
