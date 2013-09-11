b7076585efb91427bf5aa57437821e539fbc51f7
Zain <zainengineer@mrphp.com.au>
2013-09-11 17:37:03 +0930

    fixed the bug in previous commit of memcache error

--- app/app-1.dev/models/Audit.php
+++ app/app-1.dev/models/Audit.php
@@ -453,10 +453,11 @@ class Audit extends ActiveRecord
         // create new Audit
         self::$_audit = new Audit();
         //cache not working so it could not get schema for audits
-        if (!isset(self::$_audit->created)){
+        if (!self::$_audit->attributes){
             return false;
         }
-        if (self::$_audit->recordAudit()) {
+        if
+            if (self::$_audit->recordAudit()) {
             Yii::app()->onEndRequest = array(self::$_audit, 'updateAudit');
         }
         return self::$_audit;
